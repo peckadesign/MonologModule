1ceeceef74b67f89c2b891760e8927167a67c6c9
Alois Jaša <aloisjasa@gmail.com>
2023-01-02 11:49:47 +0100

    Bump monolog/monolog 2.1.* >>> 2.8.*, - bump nette/bootstrap 3.1, - bump nette/tester 2.4.*, - zrušeno $expandNewlines bez náhrady

--- src/Handlers/DayFileHandler.php
+++ src/Handlers/DayFileHandler.php
@@ -7,6 +7,8 @@ namespace Pd\MonologModule\Handlers;
  *
  *  - Výsledná cesta je %logDir%/názevKanálu/YYYY-MM/YYYY-MM-DD-názevKanálu.log
  *  - Adresář pro uložení souboru se vytváří automaticky
+ *
+ * @phpstan-import-type FormattedRecord from \Monolog\Handler\AbstractProcessingHandler
  */
 final class DayFileHandler extends \Monolog\Handler\AbstractProcessingHandler
 {
@@ -19,68 +21,64 @@ final class DayFileHandler extends \Monolog\Handler\AbstractProcessingHandler
 
 	private string $appName;
 
-	private bool $expandNewlines;
 
-
-	public function __construct(string $appName, string $logDir, bool $expandNewlines = FALSE)
+	public function __construct(string $appName, string $logDir)
 	{
 		parent::__construct();
 
 		$this->logDir = $logDir;
 		$this->appName = $appName;
-		$this->expandNewlines = $expandNewlines;
 
 		$this->defaultFormatter = new \Monolog\Formatter\LineFormatter('[%datetime%] %message% %context% %extra%');
 		$this->priorityFormatter = new \Monolog\Formatter\LineFormatter('[%datetime%] %level_name%: %message% %context% %extra%');
 	}
 
 
-	/**
-	 * @param array<mixed> $record
-	 */
 	public function handle(array $record): bool
 	{
 		if ($record['channel'] === $this->appName) {
 			$this->setFormatter($this->defaultFormatter);
-			$record['filename'] = \strtolower($record['level_name']);
 		} else {
 			$this->setFormatter($this->priorityFormatter);
-			$record['filename'] = \strtolower($record['channel']);
 		}
 
 		return parent::handle($record);
 	}
 
 
-	private function getFileName(\DateTimeInterface $dateTime, string $fileName): string
+	/**
+	 * @param array<mixed> $record
+	 */
+	private function getFileName(array $record): string
 	{
-		$pathParts = [
+		return $record['channel'] === $this->appName ? $record['level_name'] : $record['channel'];
+	}
+
+
+	private function getFilePath(\DateTimeInterface $dateTime, string $fileName): string
+	{
+		return \sprintf('%s/%s/%s/%s-%s.log',
+			$this->logDir,
 			$fileName,
 			$dateTime->format('Y-m'),
-			$dateTime->format('Y-m-d') . '-' . $fileName,
-		];
-
-		return '/' . \implode('/', $pathParts) . '.log';
+			$dateTime->format('Y-m-d'),
+			$fileName
+		);
 	}
 
 
 	/**
 	 * @param array<mixed> $record
+	 *
+	 * @phpstan-param FormattedRecord $record
 	 */
 	protected function write(array $record): void
 	{
-		$filePath = $this->logDir . $this->getFileName($record['datetime'], $record['filename']);
+		$filePath = $this->getFilePath($record['datetime'], $this->getFileName($record));
 		$logDirectory = \dirname($filePath);
 		\Nette\Utils\FileSystem::createDir($logDirectory);
 
-		if ($this->expandNewlines) {
-			$entry = '';
-			foreach ((array) \preg_split('{[\r\n]+}', (string) $record['message']) as $line) {
-				$entry .= \trim($this->getFormatter()->format(['message' => $line] + $record)) . \PHP_EOL;
-			}
-		} else {
-			$entry = \preg_replace('#\s*\r?\n\s*#', ' ', \trim($record['formatted'])) . \PHP_EOL;
-		}
+		$entry = \preg_replace('#\s*\r?\n\s*#', ' ', \trim($record['formatted'])) . \PHP_EOL;
 
 		if ( ! @\file_put_contents($filePath, $entry, \FILE_APPEND | \LOCK_EX)) {
 			throw new \RuntimeException(\sprintf('Unable to write to log file %s. Is directory writable?', $filePath));
